43ddf122b123b7585160ee347a8c23679ebb5a83
Dawid Jaracz <dawid.jaracz@gmail.com>
2017-07-23 21:37:40 +0200

    webpack 2 starter

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import _ from 'lodash';
+import './styles/style.css';
+import './styles/scssStyles.scss';
 
 function component() {
     var element = document.createElement('div');
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    // element.classList.add('hello');
+
+    element.innerHTML = _.join(['Hello', 'world'], ' ');
+    element.classList.add('helloScss');
+
+    var testEs6 = [1, 2, 3];
+    console.log(...testEs6);
 
     return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,74 @@
 var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    context: path.resolve(__dirname, './src'),
+    entry: [
+        'babel-polyfill',
+        path.join(__dirname, './src/index.js')
+    ],
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        publicPath: '/',
+        compress: true,
+        port: 9000,
+        hot: true
+    },
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Hello World!'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(sass|scss)$/,
+                exclude: /node_modules/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                exclude: /node_modules/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                exclude: /node_modules/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015']
+                    }
+                }]
+            }
+        ]
     }
 };
